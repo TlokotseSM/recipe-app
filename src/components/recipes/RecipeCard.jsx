2a1e4895af85d981d9caee03f29340a7ed56ce95
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-30 13:49:14 +0200

    decreased the recipe image size on the landing cart

--- src/components/recipes/RecipeCard.jsx
+++ src/components/recipes/RecipeCard.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import '../../index.css'
 
 const RecipeCard = ({ recipe }) => {
   return (
     <div className="recipe-card">
-      <img src={recipe.image} alt={recipe.name} />
+      {/* <img src={recipe.image} alt={recipe.name} /> */}
+      <img src={recipe.image} alt={recipe.name} style={{ width: '200px', height: '200px', objectFit: 'cover', borderRadius: '8px' }} 
+/>
       <div className="recipe-info">
         <h3>{recipe.name}</h3>
         <p>Cuisine: {recipe.cuisine}</p>
